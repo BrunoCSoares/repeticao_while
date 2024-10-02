260c8080ba92397542b2d0dcc2a7f2dc35d01bd1
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-10-01 22:30:36 -0300

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,12 @@
+'''
+Mostrar todos os inteiros entre dois números digitados pelo usuário. Exemplo: usuário digita os números 8 e 15, e aparecem em tela: 9, 10, 11, 12, 13, 14.
+'''
+
+inicio = int(input("Digite o início do intervalo: "))
+fim = int(input("Digite o fim do intervalo: "))
+
+if inicio < fim:
+    cont = inicio + 1
+    while cont <= fim - 1:
+        print(cont)
+        cont += 1
