8c76496ece17987ce615be8666a2c379cd3c0d4a
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-10-16 14:09:54 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -2,11 +2,12 @@
 Desenvolva um programa que solicite números inteiros ao usuário e calcule a soma deles até que o 0 seja digitado.
 '''
 
-num = 8
 soma = 0
 
+num = int(input("Insira um número inteiro: "))
+
 while num != 0:
-    num = int(input("Insira um número inteiro: "))
     soma += num
+    num = int(input("Insira um número inteiro: "))
 
 print(soma)
